c9d2030264b2ee408a457d06deb2b706fddee8ce
Stackbit Code Editor <projects@stackbit.com>
2021-03-28 14:07:17 +0000

    tailwind.config.js: changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@ const { colors } = require('tailwindcss/defaultTheme');
 module.exports = {
   theme: {
     colors: {
-      primary: colors.pink,
-      secondary: colors.yellow,
+      primary: colors.teal,
+      secondary: colors.orange,
       neutral: colors.gray,
       white: '#FFF',
     },
